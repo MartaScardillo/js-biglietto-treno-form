9465ed92203d133f3f4286ee31317bcc58a86957
Marta Scardillo <martascardillo02@gmail.com>
2023-10-05 21:11:24 +0200

    Add some decorative stuff

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const KM_FEE = 0.21;
 const DISCOUNT_MINORS = 20;
 const DISCOUNT_OVER_65 = 40;
 
-let discount = 0;
 let ticketType = 'Standard'
 
 const fullNameDOMElement = document.getElementById("fullName");
@@ -14,7 +13,8 @@ const ticketDOMElement = document.getElementById('ticket')
 // RICHIAMO L'INPUT 'GENERA'
 const generateButtonDOMElement = document.getElementById("generateButton");
 generateButtonDOMElement.addEventListener("click", function () {
-  // RACCOLGO I VALORI DEGLI INPUT
+    // RACCOLGO I VALORI DEGLI INPUT
+    let discount = 0;
   const fullName = fullNameDOMElement.value;
   console.log(fullName);
   const km = parseFloat(kmDOMElement.value);
@@ -45,28 +45,31 @@ generateButtonDOMElement.addEventListener("click", function () {
   console.log(coachRandom)
 
   ticketDOMElement.innerHTML = 
-  '<h2>IL TUO BIGLIETTO</h2>' +
-  '<div>' +
-      '<h3>DETTAGLIO PASSEGGERO</h3>' +
+  '<div class="pt-5">' +
+  '<h2 class="d-flex justify-content-center pb-5">IL TUO BIGLIETTO</h2>' +
+  '<div class="container bg-ticket d-flex flex-column align-items-center pb-5">' +
+  '<h2 class="pt-5">POLAR EXPRESS</h2>' +
+  '<h3 class="pb-4">ROUND TRIP</h3>' +
       '<table>' +
           '<thead>' +
               '<tr>' +
-                  '<th class="bg-gray">Nome Passeggero</th>' +
-                  '<th>Offerta</th>' +
-                  '<th>Carrozza</th>' +
-                  '<th>Codice CP</th>' +
-                  '<th>Costo Biglietto</th>' +
+                  '<th class="px-3 pb-3">Nome Passeggero</th>' +
+                  '<th class="px-3 pb-3">Offerta</th>' +
+                  '<th class="px-3 pb-3">Carrozza</th>' +
+                  '<th class="px-3 pb-3">Codice CP</th>' +
+                  '<th class="px-3 pb-3">Costo Biglietto</th>' +
               '</tr>' +
           '</thead>' +
           '<tbody>' +
               '<tr>' +
-                  '<th>' + fullName + '</th>' +
-                  '<td> Biglietto ' + ticketType + '</td>' +
-                  '<td>' + coachRandom + '</td>' +
-                  '<td>' + codeRandom + '</td>' +
-                  '<td>' + finalPrice + '€ </td>' +
+                  '<th class="text-center">' + fullName + '</th>' +
+                  '<td class="text-center"> Biglietto ' + ticketType + '</td>' +
+                  '<td class="text-center">' + coachRandom + '</td>' +
+                  '<td class="text-center">' + codeRandom + '</td>' +
+                  '<td class="text-center">' + finalPrice + '€ </td>' +
               '</tr>' +
           '</tbody>' +
       '</table>' +
+  '</div>' +
   '</div>'
 });
